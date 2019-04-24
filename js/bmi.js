987d301a0af990b1194ebdd87c01c4e48a1e269c
HuongDP <huongdp051092@gmail.com>
2019-04-25 00:44:33 +0700

    upload assignment bmi

---
+++ js/bmi.js
@@ -0,0 +1,14 @@
+let weight = parseInt(prompt("Input Weight "));
+let height = parseInt(prompt("Input Height ")) / 100;
+function bmi() {
+    bmi = weight / (height*height);
+    if (bmi<18.5) {
+        document.write("Underweight");
+    } else if (bmi < 25.0) {
+        document.write("Normal");
+    } else if (bmi < 30.0) {
+        document.write("Overweight");
+    } else
+        document.write("Obese");
+}
+bmi();
